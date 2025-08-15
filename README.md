66029e6e207c195d2534001f235483898bc3021a
jacob <jacobgraham@woosterbrush.com>
2025-08-15 15:29:38 -0400

    Initial commit!

---
+++ README.md
@@ -0,0 +1,5 @@
+# ScryWP Search
+
+Be sure to run `setup.sh` to fully set up the plugin on your behalf!  Then just follow the prompts.
+
+A powerful semantic search plugin with advanced filtering and sorting features.
---
+++ scrywp_search.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name:	ScryWP Search
+ * Plugin URI:	https://scrywp.com
+ * Description:	A powerful semantic search plugin with advanced filtering and sorting features.
+ * Version:	1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:	JG Web Development
+ * Author URI:	https://jacob-t-graham.com
+ * License:	GPLv3 or later
+ * License URI:	https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:	scry-wp
+ */
+
+//exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Require Composer's autoload file
+require_once plugin_dir_path(__FILE__) . '/vendor/autoload.php';
+
+// Use statements for namespaced classes
+use jtgraham38\jgwordpresskit\Plugin;
+use jtgraham38\jgwordpresskit\PluginFeature;
+
+//create a new plugin manager
+$plugin = new Plugin("scrywp_", plugin_dir_path( __FILE__ ), plugin_dir_url( __FILE__ ));
+
+//register features with the plugin manager here...
+//TODO: $feature = ... (import and instantiate a PluginFeature() subclass);
+//TODO: $plugin->register_feature("feature_key", $feature);
+
+//init the plugin
+$plugin->init();
