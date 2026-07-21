a679d8e2f5eb566c8f7807eb08a7417d22bb1592
Jon Graham <jagraham30@gmail.com>
2026-07-21 11:55:41 -0400

    Hook into search query to request highlighted results.

--- features/highlighting/feature.php
+++ features/highlighting/feature.php
@@ -8,6 +8,7 @@ if (!defined('ABSPATH')) {
 require_once plugin_dir_path(__FILE__) . '../../vendor/autoload.php';
 
 use jtgraham38\jgwordpresskit\PluginFeature;
+use Meilisearch\Contracts\SearchQuery;
 
 /**
  * Adds matched-term highlighting to Scry Search results.
@@ -18,7 +19,10 @@ class ScrySearch_HighlightingFeature extends PluginFeature {
      * Register feature filters.
      */
     public function add_filters() {
-        // Highlighting filters will be registered in the next implementation step.
+        add_filter(
+            $this->prefixed('multi_search_query'),
+            array($this, 'add_highlight_options')
+        );
     }
 
     /**
@@ -26,5 +30,15 @@ class ScrySearch_HighlightingFeature extends PluginFeature {
      */
     public function add_actions() {
         // Settings and frontend assets will be registered in later steps.
+    }
+
+    /**
+     * Ask Meilisearch to return highlighted title and excerpt values.
+     */
+    public function add_highlight_options(SearchQuery $search_query): SearchQuery {
+        return $search_query
+            ->setAttributesToHighlight(array('post_title', 'post_excerpt'))
+            ->setHighlightPreTag('<mark class="scry-ms-highlight">')
+            ->setHighlightPostTag('</mark>');
     }
 }
